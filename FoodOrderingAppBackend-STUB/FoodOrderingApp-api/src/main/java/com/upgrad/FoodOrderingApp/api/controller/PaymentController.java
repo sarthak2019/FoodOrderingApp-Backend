7e20729bf98523b39c0cf5e3454d5c4cb3d3b8b8
chetan-rc <chetan4195@gmail.com>
2019-09-08 22:49:37 +0530

    PaymentController created. Need to change rendered response structure and add Exception handling

---
+++ FoodOrderingAppBackend-STUB/FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/PaymentController.java
@@ -0,0 +1,51 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.model.PaymentListResponse;
+import com.upgrad.FoodOrderingApp.api.model.PaymentResponse;
+import com.upgrad.FoodOrderingApp.service.businness.PaymentService;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @RequestMapping(method= RequestMethod.GET, path= "/payment" , produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<PaymentListResponse>> payment() throws NullPointerException{
+        //final PaymentEntity paymentEntity= new PaymentEntity();
+        //final PaymentListResponse> paymentListResponses = new ArrayList<PaymentListResponse>();
+
+            final List<PaymentEntity> paymentMethods = paymentService.paymentMethods();
+
+        final List<PaymentListResponse> paymentListResponses = new ArrayList<PaymentListResponse>();
+        //final PaymentListResponse paymentListResponses = new PaymentListResponse();
+        //final List<PaymentEntity> paymentMethods=paymentService.paymentMethods(paymentEntity);
+        //final List<PaymentEntity> paymentMethods=
+               //System.out.println(paymentService.paymentMethods(paymentEntity));
+
+        for(PaymentEntity paymentEntity: paymentMethods){
+            //PaymentListResponse paymentListResponse=new PaymentListResponse();
+            //paymentListResponses.add(paymentListResponse);
+            PaymentResponse paymentResponse=new PaymentResponse().paymentName(paymentEntity.getPayment());
+            paymentResponse.id((UUID.fromString(paymentEntity.getUuid())));
+            PaymentListResponse paymentListResponse=new PaymentListResponse().addPaymentMethodsItem(paymentResponse);
+            paymentListResponses.add(paymentListResponse);
+        }
+        System.out.println(paymentListResponses);
+        return new ResponseEntity<List<PaymentListResponse>>(paymentListResponses, HttpStatus.OK);
+        //return new ResponseEntity(HttpStatus.OK);
+    }
+}
